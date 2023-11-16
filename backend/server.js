4cc7c95c7ad0e146496e159694ab35b364f8877e
SharonShaju29 <sharonfreakz29@gmail.com>
2023-11-17 00:29:23 +0530

    Added for sockket issue fix

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origins: ["http://localhost:3000","https://socialize-6s8z.onrender.com"]
   },
 });
 
